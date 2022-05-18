686b532c4d78a7e407bfd6729ede324c5ad35b44
nk400 <navkr007@gmail.com>
2022-05-18 05:40:45 +0530

    ADDED: Welcome message

---
+++ src/com/bl/basiccoreprograms/BasicCorePrograms.java
@@ -0,0 +1,7 @@
+package com.bl.basiccoreprograms;
+
+public class BasicCorePrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the section of Basic core programs");
+    }
+}
