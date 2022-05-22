9153f6eae49dde026713a2bb924211dc7261df8c
nk400 <navkr007@gmail.com>
2022-05-22 11:01:56 +0530

    REFACTORED: Finding triplets

--- src/com/bl/functionalprograms/SumOfThree.java
+++ src/com/bl/functionalprograms/SumOfThree.java
@@ -3,42 +3,35 @@ package com.bl.functionalprograms;
 import java.util.Scanner;
 
 public class SumOfThree {
+
     public static void main(String[] args) {
 
-        int[][][] arrayInput = new int[454][454][454];
-        int row;
-        int column;
-        int block;
+        System.out.println("Enter your Array size.");
 
         Scanner refVarInput = new Scanner(System.in);
-        System.out.println("Enter your Array size.");
-        System.out.print("Enter the row size : ");
-        row = refVarInput.nextInt();
-        System.out.print("Enter the column size :");
-        column = refVarInput.nextInt();
-        System.out.print("Enter the inside size :");
-        block = refVarInput.nextInt();
+        int arraySize = refVarInput.nextInt();
+        int[] arrayInput = new int[arraySize];
+        int countTriplets = 0;
 
-        for (int indexOfRow = 0; indexOfRow < row; indexOfRow++) {
-            for (int indexOfColumn = 0; indexOfColumn < column; indexOfColumn++) {
-                for (int indexOfInside = 0; indexOfInside < block; indexOfInside++) {
-                    System.out.print("Enter the value of (" + indexOfRow + "," + indexOfColumn + "," + indexOfInside + ")th location : ");
-                    arrayInput[indexOfRow][indexOfColumn][indexOfInside] = refVarInput.nextInt();
-                }
-            }
+        for (int index = 0; index < arraySize; index++) {
+            System.out.print("Enter thw element at location of array: [" + index + "] : ");
+            arrayInput[index] = refVarInput.nextInt();
+        }
 
-            for (int indexOfRows = 0; indexOfRows < row; indexOfRows++) {
-                for (int indexOfColumn = 0; indexOfColumn < column; indexOfColumn++) {
-                    for (int indexOfInside = 0; indexOfInside < block; indexOfInside++) {
-                        int sum = arrayInput[indexOfRow][indexOfColumn][indexOfInside] + arrayInput[indexOfRow][indexOfColumn][indexOfInside + 1] + arrayInput[indexOfRow][indexOfColumn + 1][indexOfInside + 2] + arrayInput[indexOfRows][indexOfColumn + 2][indexOfInside + 3];
-                        if (sum == 0) {
-                            System.out.println(arrayInput[indexOfRow][indexOfColumn][indexOfInside] + "," + arrayInput[indexOfRow][indexOfColumn][indexOfInside + 1] + "," + arrayInput[indexOfRows][indexOfColumn + 2][indexOfInside + 3]);
-                            System.out.println();
-                        }
+        for (int IndexI = 0; IndexI < arraySize - 2; IndexI++) {
+            for (int indexJ = IndexI + 1; indexJ < arraySize - 1; indexJ++) {
+                for (int indexK = indexJ + 1; indexK < arraySize; indexK++) {
+                    int sum = arrayInput[IndexI] + arrayInput[indexJ] + arrayInput[indexK];
+                    if (sum == 0) {
+                        ++countTriplets;
+                        System.out.println("\n \t " + arrayInput[IndexI] + "\t" + arrayInput[indexJ] + "\t" + arrayInput[indexK]);
                     }
                 }
             }
         }
+        if (countTriplets == 0)
+            System.out.println(" \n No distinct triplets found in the array");
+        else
+            System.out.println(" Total number of Distinct triplets in the array : " + countTriplets);
     }
 }
-
--- src/com/bl/functionalprograms/TwoDimensionalArray.java
+++ src/com/bl/functionalprograms/TwoDimensionalArray.java
@@ -4,65 +4,61 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class TwoDimensionalArray {
+class TwoDimensionalArray {
 
     static final int FOR_INTEGER = 1;
     static final int FOR_DOUBLE = 2;
     static final int FOR_BOOLEAN = 3;
     static final int TERMINATION = 4;
 
+    int row;
+    int column;
+    int[][] arrayOfInt = new int[545][545];
+    double[][] arrayOfDouble = new double[545][545];
+    boolean[][] arrayOfBoolean = new boolean[2][2];
+    Scanner refVarInput = new Scanner(System.in);
+    static Scanner choice = new Scanner(System.in);
+    static int choiceOfUsers = 0;
+
     public static void main(String[] args) {
 
         int rows;
         int columns;
+        TwoDimensionalArray refVar = new TwoDimensionalArray();
 
-        AllInputOutput refVar = new AllInputOutput();
-        Scanner choice = new Scanner(System.in);
         System.out.println(" \n Enter a number between 1 to 3 Only and Enter 4 to terminate from here");
-        int choiceOfUsers = choice.nextInt();
+        choiceOfUsers = choice.nextInt();
 
         while ((choiceOfUsers <= 0 || choiceOfUsers > TERMINATION)) {
             System.out.println(" \n Please Enter a number between 1 to 3 Only and Enter 4 to terminate from here");
             choiceOfUsers = choice.nextInt();
         }
 
-        while (choiceOfUsers > 0) {
+        while (choiceOfUsers > 0 && choiceOfUsers < 5) {
             switch (choiceOfUsers) {
                 case FOR_INTEGER:
-                    int[][] arrayOfInt = refVar.takeInputIntArray();
+                    System.out.println("You're taking Integer array input");
+                    int[][] arrayOfInti = refVar.takeInputIntArray();
                     rows = refVar.row;
                     columns = refVar.column;
-                    refVar.printArrayOfInt(arrayOfInt, rows, columns);
-                    System.out.println(" \n Enter 1 for Integer input, 2 for double and 3 for boolean or any other number to terminate.");
-                    choiceOfUsers = choice.nextInt();
-                    if ((choiceOfUsers < 0 || choiceOfUsers > 3)) {
-                        System.out.println("Program Terminated");
-                        return;
-                    }
+                    refVar.printArrayOfInt(arrayOfInti, rows, columns);
+                    refVar.takingInputOverCases();
                     break;
                 case FOR_DOUBLE:
+                    System.out.println("You're taking Double array input");
                     double[][] arrayOfDouble = refVar.takeInputDoubleArray();
                     rows = refVar.row;
                     columns = refVar.column;
                     refVar.printArrayOfDouble(arrayOfDouble, rows, columns);
-                    System.out.println(" \n Enter 1 for Integer input, 2 for double and 3 for boolean or any other number to terminate");
-                    choiceOfUsers = choice.nextInt();
-                    if ((choiceOfUsers < 0 || choiceOfUsers > 3)) {
-                        System.out.println("Program Terminated");
-                        return;
-                    }
+                    refVar.takingInputOverCases();
                     break;
                 case FOR_BOOLEAN:
+                    System.out.println("You're taking Boolean array input");
                     boolean[][] arrayOfBoolean = refVar.takeInputBooleans();
                     rows = refVar.row;
                     columns = refVar.column;
                     refVar.printArrayOfBooleans(arrayOfBoolean, rows, columns);
-                    System.out.println(" \n Enter 1 for Integer input, 2 for double and 3 for boolean or any other number to terminate");
-                    choiceOfUsers = choice.nextInt();
-                    if ((choiceOfUsers < 0 || choiceOfUsers > 3)) {
-                        System.out.println("Program Terminated");
-                        return;
-                    }
+                    refVar.takingInputOverCases();
                     break;
                 case TERMINATION:
                     System.out.println("your program is terminated");
@@ -70,17 +66,15 @@ public class TwoDimensionalArray {
             }
         }
     }
-}
 
-class AllInputOutput {
+    public void takingInputOverCases() {
+        System.out.println(" \n Enter 1 for Integer input, 2 for double and 3 for boolean or any other number to terminate");
 
-    int row;
-    int column;
-
-    int[][] arrayOfInt = new int[545][545];
-    double[][] arrayOfDouble = new double[545][545];
-    boolean[][] arrayOfBoolean = new boolean[2][2];
-    Scanner refVarInput = new Scanner(System.in);
+        choiceOfUsers = choice.nextInt();
+        if ((choiceOfUsers < 0 || choiceOfUsers > 3)) {
+            System.out.println("Program Terminated");
+        }
+    }
 
     public int[][] takeInputIntArray() {
 
@@ -97,10 +91,10 @@ class AllInputOutput {
 
     public void printArrayOfInt(int[][] arrayInt, int row, int column) {
 
-        OutputStreamWriter refOfKb = new OutputStreamWriter(System.out);
-        PrintWriter refOfSt = new PrintWriter(refOfKb);
+        OutputStreamWriter refOfTer = new OutputStreamWriter(System.out);
+        PrintWriter refOfSt = new PrintWriter(refOfTer);
 
-        refOfSt.println("\n\n Printing Your integer array : \n ");
+        refOfSt.println("\n\n Printing Your integer array of size " + row + "*" +column + " : " );
 
         for (int indexOfRow = 0; indexOfRow < row; indexOfRow++) {
             for (int indexOfColumn = 0; indexOfColumn < column; indexOfColumn++) {
@@ -110,7 +104,6 @@ class AllInputOutput {
         }
         refOfSt.flush();
     }
-
 
     public double[][] takeInputDoubleArray() {
 
@@ -178,6 +171,5 @@ class AllInputOutput {
         row = refVarInput.nextInt();
         System.out.print("Enter the column size :");
         column = refVarInput.nextInt();
-
     }
 }
